ddd3b397b27965f09a5bafec6ffd7262c2a813a4
b9b4ymiN <pro.thitipat@gmail.com>
2025-08-29 22:25:09 +0700

    update : remove web_hookDiscord in wallet 2

--- src/test.ts
+++ src/test.ts
@@ -7,8 +7,6 @@ const wallet: WalletConfig = {
   address: "5CoxdsuoRHDwDPVYqPoeiJxWZ588jXhpimCRJUj8FUN1",
   twProfile_img:
     "https://pbs.twimg.com/profile_images/1907294815081078784/KfZfapna_400x400.jpg",
-  discord_ch:
-    "https://discord.com/api/webhooks/1359708741129207979/C6vojuylxhUaG1ACa3qvjVM1R2RlzuNtyTjjP7YuN7P2eqV7-4mhPR0HsctLGviDrD7S",
 };
 const runTest = async () => {
   const walletTracker = new WalletTracker();
